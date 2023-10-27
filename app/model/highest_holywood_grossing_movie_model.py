a6fa8b16af716ce4bcbd513252331f50a36c6019
“Felipe <aluna1997@ciencias.unam.mx>
2023-10-27 13:56:21 -0600

    pedro

--- app/model/highest_holywood_grossing_movie_model.py
+++ app/model/highest_holywood_grossing_movie_model.py
@@ -8,6 +8,9 @@ from datetime import datetime
 from app.logger import configure_log
 logger = configure_log()
 
+# GLOBALS
+optionals = ['creation_date','active']
+
 def create_highest_holywood_grossing_movie(session,info_movie):
     '''
     
@@ -15,7 +18,6 @@ def create_highest_holywood_grossing_movie(session,info_movie):
     new_movie = HighestHolywoodGrossingMovie()
 
     for key,value in info_movie.items():
-        optionals = ['creation_date','active']
         if (not value) and (key not in optionals):
             str_err = '{key} no puede ser vacío al crear un nuevo highest_holywood_grossing_movie.'
             logger.error(str_err)
@@ -32,6 +34,44 @@ def create_highest_holywood_grossing_movie(session,info_movie):
         str_err = f'Ocurrió un error al actualizar HighestHolywoodGrossingMovie: {str(err)}.'
         logger.error(str_err)
         raise err
+    
+def read_highest_holywood_grossing_movie(session,id_movie = False):
+    
+    if not id_movie:
+        movies = session.query(HighestHolywoodGrossingMovie).all()
+    else:
+        movies = session.query(HighestHolywoodGrossingMovie).get(id_movie)
+
+    return movies
+
+def update_highest_holywood_grossing_movie(session,id_movie,info_movie):
+
+    if not id_movie:
+        str_err = 'Para actualizar, el id_movie no debe ser vacío.'
+        logger.error(str_err)
+        raise Exception(str_err)
+    
+    movie = session.query(HighestHolywoodGrossingMovie).get(id_movie)
+
+    if not movie:
+        logger.warning('El id a actualizar no existe, no se actualiza nada.')
+
+    for key,value in info_movie.items():
+        if getattr(movie, key, None):
+            setattr(movie, key, value)
+        else:
+            logger.warning(f'El atributo {key} no existe para el objeto de highest_holywood_grossing_movie, se ignora.')
+
+    try:
+        session.add(movie)
+        session.commit()
+        return movie.id_movie
+    except Exception as err:
+        session.rollback()
+        str_err = f'Ocurrió un error al actualizar HighestHolywoodGrossingMovie: {str(err)}.'
+        logger.error(str_err)
+        raise err
+    
 
 if __name__ == "__main__":
     db_conn = DatabaseConnection()
@@ -62,6 +102,10 @@ if __name__ == "__main__":
                   'movie_runtime': '2hr 12 min',
                   'license': 'NA'}
 
-    last_id = create_highest_holywood_grossing_movie(session,info_movie_3)
-    print(last_id)
+    #last_id = create_highest_holywood_grossing_movie(session,info_movie_1)
+    #print(last_id)
+
+
+    movie = read_highest_holywood_grossing_movie(session,id_movie=3)
+    print(movie.__dict__)
     db_conn.close_session()
